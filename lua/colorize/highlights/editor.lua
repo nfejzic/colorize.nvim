ffe0212617c3c6a8bc91aeb73d6d291427b264e6
Nadir Fejzić <nadir@notfloor.com>
2025-04-10 22:25:00 +0200

    fix: use nicer color for lsp hover highlight

--- lua/colorize/highlights/editor.lua
+++ lua/colorize/highlights/editor.lua
@@ -151,7 +151,7 @@ function M.setup(colors, config)
 		debugPC = { bg = theme.diff.delete },
 		debugBreakpoint = { fg = theme.syn.special1, bg = theme.ui.bg_gutter },
 
-		LspReferenceText = { bg = theme.diff.text },
+		LspReferenceText = { link = "CursorLine" },
 		LspReferenceRead = { link = "LspReferenceText" },
 		LspReferenceWrite = { bg = theme.diff.text, underline = true },
 		LspInlayHint = { link = "NonText" },
